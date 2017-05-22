50bb44c5483acdc3ad0ee3b561d5797df589baf5
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-05-22 10:56:01 +0100

    [BTAT-99] update libraries

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -24,7 +24,7 @@ object MicroServiceBuild extends Build with MicroService {
 
   def test(scope: String = "test,it") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
-    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.0" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
   )
