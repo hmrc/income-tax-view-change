229b3c24d63e1442ef45cae52128d9a3a18480df
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-05-22 11:57:07 +0100

    [BTAT-102] Modify unit tests to use DI

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import sbt._
 import play.sbt.PlayImport._
 import play.core.PlayVersion
@@ -11,7 +26,7 @@ object MicroServiceBuild extends Build with MicroService {
 
   override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % "5.15.0",
     "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
@@ -22,9 +37,10 @@ object MicroServiceBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "domain" % "4.1.0"
   )
 
-  def test(scope: String = "test,it") = Seq(
+  def test(scope: String = "test,it"): Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
     "org.scalatest" %% "scalatest" % "3.0.0" % scope,
+    "org.scalatestplus" %% "play" % "1.2.0" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
   )
--- test/controllers/MicroserviceHelloWorldControllerSpec.scala
+++ test/controllers/MicroserviceHelloWorldControllerSpec.scala
@@ -14,25 +14,22 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.incometaxviewchange.controllers
-
+package controllers
+import config.MockAppConfig
 import play.api.http.Status
 import play.api.test.FakeRequest
-import play.api.http.Status
-import play.api.test.FakeRequest
-import uk.gov.hmrc.play.test.UnitSpec
-import uk.gov.hmrc.play.test.WithFakeApplication
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 
 
-class MicroserviceHelloWorldControllerSpec extends UnitSpec with WithFakeApplication{
+class MicroserviceHelloWorldControllerSpec extends UnitSpec with WithFakeApplication {
+
+  object TestController extends MicroserviceHelloWorld()(MockAppConfig)
 
   val fakeRequest = FakeRequest("GET", "/")
 
-
   "GET /" should {
     "return 200" in {
-      val result = MicroserviceHelloWorld.hello()(fakeRequest)
+      val result = TestController.hello()(fakeRequest)
       status(result) shouldBe Status.OK
     }
   }
