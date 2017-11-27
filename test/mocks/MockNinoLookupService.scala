0eda509d4ec7ae1ae9877c69f5aecdba9083ae2e
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-11-27 13:54:35 +0000

    [BTAT-1181] add NinoLookupController

---
+++ test/mocks/MockNinoLookupService.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mocks
+
+import assets.TestConstants._
+import models.DesResponseModel
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito._
+import org.mockito.stubbing.OngoingStubbing
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.mockito.MockitoSugar
+import services.NinoLookupService
+import uk.gov.hmrc.play.test.UnitSpec
+
+import scala.concurrent.Future
+
+
+trait MockNinoLookupService extends UnitSpec with MockitoSugar with BeforeAndAfterEach {
+
+  val mockNinoLookupService: NinoLookupService = mock[NinoLookupService]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockNinoLookupService)
+  }
+
+  def setupMockNinoLookupServiceResponse(mtdRef: String)(response: DesResponseModel)
+  : OngoingStubbing[Future[DesResponseModel]] =
+    when(mockNinoLookupService
+      .getNino(
+        ArgumentMatchers.eq(mtdRef))
+      (ArgumentMatchers.any()))
+      .thenReturn(Future.successful(response))
+
+  def mockNinoLookupResponse(desResponse: DesResponseModel): OngoingStubbing[Future[DesResponseModel]] =
+    setupMockNinoLookupServiceResponse(mtdRef)(desResponse)
+}
