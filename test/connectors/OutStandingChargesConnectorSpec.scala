e2db7f1d0504415d45d286f73e92d1ba4825c977
Chandra Andem <36305926+chandraAndem@users.noreply.github.com>
2022-02-11 17:48:33 +0000

    Adding pattern matching for UTR

--- test/connectors/OutStandingChargesConnectorSpec.scala
+++ test/connectors/OutStandingChargesConnectorSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 HM Revenue & Customs
+ * Copyright 2022 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ class OutStandingChargesConnectorSpec extends TestSupport with MockHttp {
 
   "listOutStandingCharges" should {
     val idType: String = "utr"
-    val idNumber: Int = 1234567890
+    val idNumber = "1234567890"
     val taxYearEndDate: String = "2020-04-05"
 
     import TestOutStandingChargesConnector.{listOutStandingCharges, listOutStandingChargesUrl}
