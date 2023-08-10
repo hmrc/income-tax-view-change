849320d08e6d07fcfa4b94166f510205c40f5fed
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2023-08-10 09:46:17 +0100

    Added cashOrAccruals to the PropertyDetailsModel

--- app/models/incomeSourceDetails/PropertyDetailsModel.scala
+++ app/models/incomeSourceDetails/PropertyDetailsModel.scala
@@ -30,7 +30,8 @@ case class PropertyDetailsModel(incomeSourceId: String,
                                 paperless: Option[Boolean],
                                 firstAccountingPeriodEndDate: Option[LocalDate],
                                 incomeSourceType: Option[String],
-                                tradingStartDate: Option[LocalDate])
+                                tradingStartDate: Option[LocalDate],
+                                cashOrAccruals: Option[String])
 
 object PropertyDetailsModel extends CustomReads {
 
@@ -47,7 +48,8 @@ object PropertyDetailsModel extends CustomReads {
       (__ \ "paperLess").readNullable[Boolean] and
       (__ \ "firstAccountingPeriodEndDate").readNullable[LocalDate] and
       (__ \ "incomeSourceType").readNullable[String] and
-      (__ \ "tradingStartDate").readNullable[LocalDate]
+      (__ \ "tradingStartDate").readNullable[LocalDate] and
+      (__ \ "cashOrAccruals").readNullable[String]
     ) (PropertyDetailsModel.applyWithFields _)
 
   def applyWithFields(incomeSourceId: String,
@@ -62,7 +64,8 @@ object PropertyDetailsModel extends CustomReads {
                       paperless: Option[Boolean],
                       firstAccountingPeriodEndDate: Option[LocalDate],
                       incomeSourceType: Option[String],
-                      tradingStartDate: Option[LocalDate]): PropertyDetailsModel = PropertyDetailsModel(
+                      tradingStartDate: Option[LocalDate],
+                      cashOrAccruals: Option[String]): PropertyDetailsModel = PropertyDetailsModel(
     incomeSourceId,
     accountingPeriod,
     ContactDetailsModel.propertyContactDetails(email),
@@ -71,7 +74,8 @@ object PropertyDetailsModel extends CustomReads {
     paperless,
     firstAccountingPeriodEndDate,
     incomeSourceType,
-    tradingStartDate
+    tradingStartDate,
+    cashOrAccruals
   )
 
   implicit val writes: OWrites[PropertyDetailsModel] = Json.writes[PropertyDetailsModel]
