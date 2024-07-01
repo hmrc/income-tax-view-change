da941ac6eb8b3e8e61ac238353075d2ea39cfe87
james-king-hmrc <165788042+james-king-hmrc@users.noreply.github.com>
2024-07-01 10:07:05 +0100

    fix indentation

--- app/models/reportDeadlines/ReportDeadlineModel.scala
+++ app/models/reportDeadlines/ReportDeadlineModel.scala
@@ -30,50 +30,51 @@
    * limitations under the License.
    */
 
-  package models.reportDeadlines
+package models.reportDeadlines
 
-  import models.reportDeadlines.ObligationStatus.{Fulfilled, Open}
+import models.reportDeadlines.ObligationStatus.{Fulfilled, Open}
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
-  import java.time.LocalDate
-  import play.api.libs.functional.syntax._
-  import play.api.libs.json.{Json, Reads, _}
+import java.time.LocalDate
 
-  case class ObligationStatus(code: String, name: String)
-  object ObligationStatus {
-    val Open = ObligationStatus("O", "Open")
-    val Fulfilled = ObligationStatus("F", "Fulfilled")
-  }
+case class ObligationStatus(code: String, name: String)
 
-  case class ReportDeadlineModel( start: LocalDate,
-                                  end: LocalDate,
-                                  due: LocalDate,
-                                  periodKey: String,
-                                  dateReceived: Option[LocalDate],
-                                  obligationType: String,
-                                  status: String
-                                )
+object ObligationStatus {
+  val Open = ObligationStatus("O", "Open")
+  val Fulfilled = ObligationStatus("F", "Fulfilled")
+}
 
-  object ReportDeadlineModel {
+case class ReportDeadlineModel(start: LocalDate,
+                               end: LocalDate,
+                               due: LocalDate,
+                               periodKey: String,
+                               dateReceived: Option[LocalDate],
+                               obligationType: String,
+                               status: String
+                              )
 
-    def desReadsApi(incomeSourceType: String): Reads[ReportDeadlineModel] = (
-      (__ \ "inboundCorrespondenceFromDate").read[LocalDate] and
-        (__ \ "inboundCorrespondenceToDate").read[LocalDate] and
-        (__ \ "inboundCorrespondenceDueDate").read[LocalDate] and
-        (__ \ "periodKey").read[String] and
-        (__ \ "inboundCorrespondenceDateReceived").readNullable[LocalDate] and
-        (incomeSourceType match {
-          case "ITSA" => Reads.pure("Crystallised")
-          case _ => (__ \ "periodKey").read[String].map {
-            case "EOPS" => "EOPS"
-            case _ => "Quarterly"
-          }
-        }) and
-        (__ \ "status").read[String].map {
-          case Open.code => Open.name
-          case Fulfilled.code => Fulfilled.name
-          case v => v
+object ReportDeadlineModel {
+
+  def desReadsApi(incomeSourceType: String): Reads[ReportDeadlineModel] = (
+    (__ \ "inboundCorrespondenceFromDate").read[LocalDate] and
+      (__ \ "inboundCorrespondenceToDate").read[LocalDate] and
+      (__ \ "inboundCorrespondenceDueDate").read[LocalDate] and
+      (__ \ "periodKey").read[String] and
+      (__ \ "inboundCorrespondenceDateReceived").readNullable[LocalDate] and
+      (incomeSourceType match {
+        case "ITSA" => Reads.pure("Crystallised")
+        case _ => (__ \ "periodKey").read[String].map {
+          case "EOPS" => "EOPS"
+          case _ => "Quarterly"
         }
-      ) (ReportDeadlineModel.apply _)
+      }) and
+      (__ \ "status").read[String].map {
+        case Open.code => Open.name
+        case Fulfilled.code => Fulfilled.name
+        case v => v
+      }
+    )(ReportDeadlineModel.apply _)
 
-    implicit val format: Format[ReportDeadlineModel] = Json.format[ReportDeadlineModel]
-  }
+  implicit val format: Format[ReportDeadlineModel] = Json.format[ReportDeadlineModel]
+}
