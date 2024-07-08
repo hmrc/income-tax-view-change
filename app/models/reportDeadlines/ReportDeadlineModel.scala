954876c6ce3425d040a757bfb1b2d03582872e16
james-king-hmrc <165788042+james-king-hmrc@users.noreply.github.com>
2024-07-08 14:33:00 +0100

    revert change

--- app/models/reportDeadlines/ReportDeadlineModel.scala
+++ app/models/reportDeadlines/ReportDeadlineModel.scala
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package models.reportDeadlines
 
 import models.reportDeadlines.ObligationStatus.{Fulfilled, Open}
