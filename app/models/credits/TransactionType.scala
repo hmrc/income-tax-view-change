e859f24bdb0a8d15a4ced931fba668f2eb61de62
Owen Gilfellon <43992493+owen-gilfellon@users.noreply.github.com>
2024-07-19 09:44:49 +0100

    MISUV-7891 | Refactor CreditsAndRefunds (#292)
    * MISUV-7891 | Added credits endpoint to return minimal credit and refunds model
    
    * MISUV-7891 | Renamed creditType to transactionType
    
    * MISUV-7891 | Tidying

---
+++ app/models/credits/TransactionType.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.credits
+
+import play.api.libs.json._
+
+sealed trait TransactionType {
+  val key: String
+}
+
+case object MfaCreditType extends TransactionType {
+  override val key = "mfa"
+}
+
+case object CutOverCreditType extends TransactionType {
+  override val key = "cutOver"
+}
+
+case object BalancingChargeCreditType extends TransactionType {
+  override val key = "balancingCharge"
+}
+
+case object RepaymentInterest extends TransactionType {
+  override val key = "repaymentInterest"
+}
+
+case object PaymentType extends TransactionType {
+  override val key = "payment"
+}
+
+case object Repayment extends TransactionType {
+  override val key = "refund"
+}
+
+object TransactionType {
+
+  implicit val write: Writes[TransactionType] = new Writes[TransactionType] {
+    def writes(transactionType: TransactionType): JsValue = {
+      JsString(transactionType.key)
+    }
+  }
+
+  val read: Reads[TransactionType] = (JsPath).read[String].collect(JsonValidationError("Could not parse transactionType")) {
+    case MfaCreditType.key => MfaCreditType
+    case CutOverCreditType.key => CutOverCreditType
+    case BalancingChargeCreditType.key => BalancingChargeCreditType
+    case RepaymentInterest.key => RepaymentInterest
+    case PaymentType.key => PaymentType
+    case Repayment.key => Repayment
+  }
+
+  implicit val format: Format[TransactionType] = Format( read, write)
+
+  // values come from EPID #1138
+  private val cutOver = "6110"
+  private val balancingCharge = "4905"
+  private val repaymentInterest = "6020"
+  private val mfaCredit = Range.inclusive(4004, 4025)
+    .filterNot(_ == 4010).filterNot(_ == 4020).map(_.toString)
+    .toList
+  private val payment = List("0060")
+
+  def fromCode(mainTransaction: String): Option[TransactionType] = {
+    mainTransaction match {
+      case TransactionType.cutOver =>
+        Some(CutOverCreditType)
+      case TransactionType.balancingCharge =>
+        Some(BalancingChargeCreditType)
+      case TransactionType.repaymentInterest =>
+        Some(RepaymentInterest)
+      case x if mfaCredit.contains(x) =>
+        Some(MfaCreditType)
+      case x if payment.contains(x) =>
+        Some(PaymentType)
+      case _ => None
+    }
+  }
+}
---
+++ test/models/credits/CreditsModelSpec.scala
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.credits
+
+import org.scalatest.matchers.should.Matchers
+import play.api.libs.json.{JsArray, JsNumber, JsString, Json}
+import utils.{AChargesResponse, TestSupport}
+
+import java.time.LocalDate
+
+class CreditsModelSpec extends TestSupport with Matchers {
+
+  "format" should {
+
+    "serialize to JSON" in {
+
+      val chargesResponse = AChargesResponse()
+        .withAvailableCredit(200.0)
+        .withAllocatedCredit(100.0)
+        .withFirstRefundRequest(200.0)
+        .withSecondRefundRequest(100.0)
+        .withCutoverCredit("CUTOVER01", LocalDate.of(2024, 6, 20), -100.0)
+        .withBalancingChargeCredit("BALANCING01", LocalDate.of(2024, 6, 19), -200)
+        .withMfaCredit("MFA01", LocalDate.of(2024, 6, 18), -300)
+        .withPayment("PAYMENT01", LocalDate.of(2024, 6, 17), -400)
+        .withRepaymentInterest("INTEREST01", LocalDate.of(2024, 6, 16), -500)
+        .get()
+
+      val creditsModel = CreditsModel.fromChargesResponse(chargesResponse)
+
+        val json = Json.toJson(creditsModel)
+
+        (json \ "availableCredit").get shouldBe JsNumber(200.0)
+        (json \ "allocatedCredit").get shouldBe JsNumber(100.0)
+
+        val transactions = (json \ "transactions").get
+        transactions match {
+          case r: JsArray =>
+            r.value.size shouldBe 7
+          case _ => fail("transactions should be JsArray")
+        }
+
+        (json \ "transactions" \ 0 \ "transactionType").get shouldBe JsString("cutOver")
+        (json \ "transactions" \ 1 \ "transactionType").get shouldBe JsString("balancingCharge")
+        (json \ "transactions" \ 2 \ "transactionType").get shouldBe JsString("mfa")
+        (json \ "transactions" \ 3 \ "transactionType").get shouldBe JsString("payment")
+        (json \ "transactions" \ 4 \ "transactionType").get shouldBe JsString("repaymentInterest")
+        (json \ "transactions" \ 5 \ "transactionType").get shouldBe JsString("refund")
+        (json \ "transactions" \ 6 \ "transactionType").get shouldBe JsString("refund")
+    }
+  }
+
+  "fromChargesResponse" should {
+
+    "parse document details and financial documents as transactions" in {
+
+      val chargesResponse = AChargesResponse()
+        .withCutoverCredit("CUTOVER01", LocalDate.of(2024, 6, 20), -100.0)
+        .withBalancingChargeCredit("BALANCING01", LocalDate.of(2024, 6, 19), -200)
+        .withMfaCredit("MFA01", LocalDate.of(2024, 6, 18), -300)
+        .withPayment("PAYMENT01", LocalDate.of(2024, 6, 17), -400)
+        .withRepaymentInterest("INTEREST01", LocalDate.of(2024, 6, 16), -500)
+        .get()
+
+      val creditsModel = CreditsModel.fromChargesResponse(chargesResponse)
+
+      creditsModel.transactions.size shouldBe 5
+      creditsModel.transactions.head shouldBe Transaction(CutOverCreditType, 100.0, Some("2024"), Some(LocalDate.of(2024, 6, 20)))
+      creditsModel.transactions(1) shouldBe Transaction(BalancingChargeCreditType, 200.0, Some("2024"), Some(LocalDate.of(2024, 6, 19)))
+      creditsModel.transactions(2) shouldBe Transaction(MfaCreditType, 300.0, Some("2024"), Some(LocalDate.of(2024, 6, 18)))
+      creditsModel.transactions(3) shouldBe Transaction(PaymentType, 400.0, Some("2024"), Some(LocalDate.of(2024, 6, 17)))
+      creditsModel.transactions(4) shouldBe Transaction(RepaymentInterest, 500.0, Some("2024"), Some(LocalDate.of(2024, 6, 16)))
+    }
+
+    "parse refund requests as transactions" in {
+      val chargesResponse = AChargesResponse()
+        .withFirstRefundRequest(200.0)
+        .withSecondRefundRequest(100.0)
+        .get()
+
+      val creditsModel = CreditsModel.fromChargesResponse(chargesResponse)
+
+      creditsModel.transactions.size shouldBe 2
+      creditsModel.transactions.foreach(t => t.transactionType shouldBe Repayment)
+      creditsModel.transactions.head.amount shouldBe 200.0
+      creditsModel.transactions(1).amount shouldBe 100.0
+      creditsModel.transactions.foreach(t => t.transactionType shouldBe Repayment)
+    }
+
+    "convert negative amounts of outstanding amount (i.e. credit) to absolute values" in {
+
+      val chargesResponse = AChargesResponse()
+        .withCutoverCredit("CUTOVER01", LocalDate.of(2024, 6, 20), -100.0)
+        .withBalancingChargeCredit("BALANCING01", LocalDate.of(2024, 6, 19), -200)
+        .withMfaCredit("MFA01", LocalDate.of(2024, 6, 18), -300)
+        .withPayment("PAYMENT01", LocalDate.of(2024, 6, 17), -400)
+        .withRepaymentInterest("INTEREST01", LocalDate.of(2024, 6, 16), -500)
+        .get()
+
+      val creditsModel = CreditsModel.fromChargesResponse(chargesResponse)
+
+      creditsModel.transactions.size shouldBe 5
+      creditsModel.transactions.forall(t => t.amount > 0) shouldBe true
+    }
+
+    // existing behaviour moved from frontend
+    "clamp positive amounts of outstanding amount to 0" in {
+
+      val chargesResponse = AChargesResponse()
+        .withCutoverCredit("CUTOVER01", LocalDate.of(2024, 6, 20), 100.0)
+        .withBalancingChargeCredit("BALANCING01", LocalDate.of(2024, 6, 19), 200)
+        .withMfaCredit("MFA01", LocalDate.of(2024, 6, 18), 300)
+        .withPayment("PAYMENT01", LocalDate.of(2024, 6, 17), 400)
+        .withRepaymentInterest("INTEREST01", LocalDate.of(2024, 6, 16), 500)
+        .get()
+
+      val creditsModel = CreditsModel.fromChargesResponse(chargesResponse)
+
+      creditsModel.transactions.size shouldBe 5
+      creditsModel.transactions.forall(t => t.amount == 0) shouldBe true
+    }
+
+    "ignore transactions when mainTransaction cannot be converted to creditType" in {
+      val chargesResponse = AChargesResponse()
+        .withCustomMainTransaction(
+          transactionId = "ID1",
+          dueDate = LocalDate.of(2024, 6, 20),
+          outstandingAmount = -100.0,
+          mainType = Some("ITSA Cutover Credits"),
+          mainTransaction = Some("!!!!"))
+        .get()
+
+      val creditsModel = CreditsModel.fromChargesResponse(chargesResponse)
+
+      creditsModel.transactions.isEmpty shouldBe true
+    }
+
+    "ignore transactions without a credit mainTransaction" in {
+      val chargesResponse = AChargesResponse()
+        .withCustomMainTransaction(
+          transactionId = "ID1",
+          dueDate = LocalDate.of(2024, 6, 20),
+          outstandingAmount = -100.0,
+          mainType = None,
+          mainTransaction = None)
+        .get()
+
+      val creditsModel = CreditsModel.fromChargesResponse(chargesResponse)
+
+      creditsModel.transactions.isEmpty shouldBe true
+    }
+  }
+}
