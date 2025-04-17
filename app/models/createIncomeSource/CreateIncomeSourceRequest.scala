3727540692389b86f09237360a43f7a17082002d
jmountain-hmrc <31282623+jmountain-hmrc@users.noreply.github.com>
2025-04-17 11:26:10 +0100

    MISUV-9585 Replace validation with forall

--- app/models/createIncomeSource/CreateIncomeSourceRequest.scala
+++ app/models/createIncomeSource/CreateIncomeSourceRequest.scala
@@ -41,7 +41,7 @@ object CreateIncomeSourceRequest {
 
 final case class CreateBusinessIncomeSourceRequest(businessDetails: List[BusinessDetails]) extends CreateIncomeSourceRequest {
   require(businessDetails.length == 1, "Only single business can be created at a time")
-  require(businessDetails.head.cashOrAccrualsFlag.getOrElse("NONE").matches("^[A-Z]+$"), "Accounting method must be capitalised")
+  require(businessDetails.head.cashOrAccrualsFlag.forall(_.matches("^[A-Z]+$")), "Accounting method must be capitalised")
 }
 
 case class BusinessDetails(accountingPeriodStartDate: String,
@@ -85,7 +85,7 @@ final case class PropertyDetails(tradingStartDate: String,
                                  cashOrAccrualsFlag: Option[String],
                                  startDate: String
                                 ) {
-  require(cashOrAccrualsFlag.getOrElse("NONE").matches("^[A-Z]+$"), "Accounting method must be capitalised")
+  require(cashOrAccrualsFlag.forall(_.matches("^[A-Z]+$")), "Accounting method must be capitalised")
   require(tradingStartDate.nonEmpty, "Trading start date must be provided")
   require(tradingStartDate == startDate, "Trading start date and start date must be the same")
 }
