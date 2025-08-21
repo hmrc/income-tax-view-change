0c57de86178de3c56d3cf554d267841945ed3355
James Forster <14073802+jameshforster@users.noreply.github.com>
2025-08-21 13:07:05 +0100

    MISUV-10088 Add hip connector and feature switch for calc list TYS.

---
+++ it/test/connectors/hip/CalculationListHipConnectorISpec.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.hip
+
+import helpers.{ComponentSpecBase, WiremockHelper}
+import models.calculationList.{CalculationListModel, CalculationListResponseModel}
+import models.errors.{InvalidJsonResponse, UnexpectedJsonFormat, UnexpectedResponse}
+import play.api.http.Status._
+import play.api.libs.json.{JsValue, Json}
+
+class CalculationListHipConnectorISpec extends ComponentSpecBase {
+
+  val connector: CalculationListHipConnector = app.injector.instanceOf[CalculationListHipConnector]
+  val nino = "AA123456A"
+  val taxYearRange = "23-24"
+  val urlCalculationHipTYS = s"/income-tax/v1/$taxYearRange/view/calculations/liability/$nino"
+
+  val calculationListResponse: CalculationListResponseModel = CalculationListResponseModel(Seq(CalculationListModel(
+    calculationId = "c432a56d-e811-474c-a26a-76fc3bcaefe5",
+    calculationTimestamp = "2023-10-31T12:55:51.159Z",
+    calculationType = "finalDeclaration",
+    crystallised = Some(false)
+  )))
+
+
+
+  "CalculationListConnector" when {
+
+    ".getCalculationListTYS() is called" when {
+
+      "the response is a 200 - OK" should {
+
+        "return the calculation list response when successful for a given NINO and tax year" in {
+
+          val requestBody: JsValue = Json.parse(
+            """
+              |[
+              | {
+              |   "calculationId":"c432a56d-e811-474c-a26a-76fc3bcaefe5",
+              |   "calculationTimestamp":"2023-10-31T12:55:51.159Z",
+              |   "calculationType":"finalDeclaration",
+              |   "crystallised": false
+              | }
+              |]
+              |""".stripMargin)
+
+          WiremockHelper.stubGet(urlCalculationHipTYS, OK, requestBody.toString())
+
+          val result = connector.getCalculationListTYS(nino, taxYearRange).futureValue
+
+          result shouldBe Right(calculationListResponse)
+        }
+
+        "return an error when the request returned has errors" in {
+          val requestBody: JsValue = Json.parse(
+            """
+              |[
+              | {
+              |   "calculationId":"c432a56d-e811-474c-a26a-76fc3bcaefe5",
+              |   "calculationTimestamp":"2023-10-31T12:55:51.159Z",
+              |   "crystallised": false
+              | }
+              |]
+              |""".stripMargin)
+
+          WiremockHelper.stubGet(urlCalculationHipTYS, OK, requestBody.toString())
+
+          val result = connector.getCalculationListTYS(nino, taxYearRange).futureValue
+
+          result shouldBe Left(UnexpectedJsonFormat)
+        }
+      }
+
+      "the response is a 500 - InternalServerError" should {
+
+        "return an error when the downstream response was unexpected" in {
+
+          WiremockHelper.stubGet(urlCalculationHipTYS, INTERNAL_SERVER_ERROR, "{}")
+
+          val result = connector.getCalculationListTYS(nino, taxYearRange).futureValue
+
+          result shouldBe Left(UnexpectedResponse)
+        }
+
+        "return an error when the json returned was in an incorrect state" in {
+
+          val requestBody: JsValue = Json.parse(
+            """
+              |[
+              | {
+              |   "calculationId":"c432a56d-e811-474c-a26a-76fc3bcaefe5",
+              |   "calculationTimestamp":"2023-10-31T12:55:51.159Z",
+              |   "crystallised": false
+              | }
+              |]
+              |""".stripMargin)
+
+          WiremockHelper.stubGet(urlCalculationHipTYS, INTERNAL_SERVER_ERROR, requestBody.toString())
+
+          val result = connector.getCalculationListTYS("1234", taxYearRange).futureValue
+
+          result shouldBe Left(InvalidJsonResponse)
+        }
+      }
+    }
+  }
+}
+
---
+++ test/connectors/hip/CalculationListHipConnectorSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.hip
+
+import constants.BaseTestConstants.{testNino, testTaxYearRange}
+import constants.CalculationListTestConstants._
+import mocks.MockHttpV2
+import models.hip.{ErrorResponse, GetCalcListTYSHipApi, UnexpectedJsonResponse}
+import uk.gov.hmrc.http.HttpResponse
+import utils.TestSupport
+
+class CalculationListHipConnectorSpec extends TestSupport with MockHttpV2 {
+
+  object TestCalculationListHipConnector extends CalculationListHipConnector(mockHttpClientV2, microserviceAppConfig)
+  val platform: String = microserviceAppConfig.hipUrl
+  val url5624 = s"$platform/income-tax/v1/$testTaxYearRange/view/calculations/liability/$testNino"
+  val header5624: Seq[(String, String)] = microserviceAppConfig.getHIPHeaders(GetCalcListTYSHipApi)
+
+  lazy val mockUrl5624: HttpResponse => Unit = setupMockHttpGetWithHeaderCarrier(url5624, header5624)(_)
+  lazy val mockUrl5624Failed: Either[ErrorResponse, Nothing] => Unit =
+    setupMockHttpGetWithHeaderCarrier(url5624, header5624)(_)
+
+  "The CalculationListHipConnector" should {
+    "format API URLs correctly" when {
+      "getCalculationListTYSUrl is called" in {
+        TestCalculationListHipConnector.getCalculationListTYSHipUrl(testNino, testTaxYearRange) shouldBe url5624
+      }
+    }
+    "correlation id is in the right format" when {
+      "getHipHeaders is called" in {
+        val regex = "^[0-9a-fA-F]{8}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{12}$"
+        header5624.last._2.matches(regex) shouldBe true
+      }
+    }
+
+    "return a CalculationList model" when {
+      "calling getCalculationListTYS with a valid NINO" in {
+       mockUrl5624(successResponse)
+        TestCalculationListHipConnector.getCalculationListTYS(testNino, testTaxYearRange).futureValue shouldBe successResponse
+      }
+    }
+
+    "return an ErrorResponse model" when {
+      "calling getCalculationList and a non-success response is received" in {
+        mockUrl5624Failed(Left(UnexpectedJsonResponse))
+        TestCalculationListHipConnector.getCalculationListTYS(testNino, testTaxYearRange).futureValue shouldBe Left(UnexpectedJsonResponse)
+      }
+    }
+  }
+}
