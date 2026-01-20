c12302f550ac9ddbcd74fda527d8138481dbe168
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2026-01-20 13:29:57 +0000

    MISUV-10745: Add mock UpdateCustomerFact connector for controller tests

---
+++ test/mocks/MockUpdateCustomerFactConnector.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mocks
+
+import connectors.hip.UpdateCustomerFactConnector
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.mvc.Result
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.Future
+
+trait MockUpdateCustomerFactConnector extends MockitoSugar {
+
+  val mockUpdateCustomerFactConnector: UpdateCustomerFactConnector = mock[UpdateCustomerFactConnector]
+
+  def mockUpdateCustomerFactsToConfirmed(mtdId: String, result: Result): Unit =
+    when(mockUpdateCustomerFactConnector.updateCustomerFactsToConfirmed(eqTo(mtdId))(any[HeaderCarrier]()))
+      .thenReturn(Future.successful(result))
+}
